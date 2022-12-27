5ef59437b6d044803caf01019fa0bc5456034981
Xerxes-zhz <git@github.com>
2022-12-27 23:57:57 +0800

    new dev branch

---
+++ JAVA/Basic/p1004/Main.java
@@ -0,0 +1,11 @@
+import java.util, StreamTokenizer, BufferedReader, InputStreamReader;
+
+public class Main {
+    public static void main(String[] args){
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    }
+    int n = st.next()
+    for 
+
+
+}
